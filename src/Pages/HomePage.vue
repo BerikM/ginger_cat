14a87a5128046ae2a9a6d82203f8bfe1517954ea
Берик Мукин <blackstyle174@gmail.com>
2023-12-04 15:39:34 +0600

    update 2

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -13,13 +13,35 @@
           пушистых и пернатых друзей еще счастливее!</div>
         <div class="button-contact">Связаться с нами</div>
       </div>
+      <div class="discounted-products">
+        <div class="products-title">Товары со скидкой</div>
+        <div class="products">
+          <div class="item">
+            <ProductComponent :imageId=1 :title="'Наполнитель комкующийся \'Pi-pi-Bent\' ,10л'" />
+          </div>
+          <div class="item">
+            <ProductComponent :imageId=2 :title="'Сухой корм для крупных пород Pedigree, 13кг'" />
+          </div>
+          <div class="item">
+            <ProductComponent :imageId=3 :title="'Наполнитель древесный \'Сибирская кошка\' , 7л'" />
+          </div>
+          <div class="item">
+            <ProductComponent :imageId=4 :title="'Корм для щенков Acana Puppy, 340г'" />
+          </div>
+        </div>
+        <div class="all_discount">все товары со скидкой</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductComponent from '../components/Product.vue';
 export default {
   name: 'HomePage',
+  components: {
+    ProductComponent
+  }
 }
 </script>
 
@@ -72,5 +94,39 @@ export default {
 .button-contact:hover {
   background-color: #CF6736;
   color: #FFE5D9;
+}
+
+.discounted-products {
+  margin-top: 330px;
+}
+
+.products-title {
+  color: #99492E;
+  font-size: 48px;
+}
+
+.products {
+  margin-top: 68px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.all_discount {
+  width: 317px;
+  height: 58px;
+  border-radius: 16px;
+  border: 2px solid #EC7C26;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #EC7C26;
+  font-size: 24px;
+  margin: 0 auto;
+  margin-top: 55px;
+}
+
+.all_discount:hover {
+  color: #FFE5D9;
+  background-color: #CF6736;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="product">
+        <div class="image"><img :src="getImagePath()" alt=""></div>
+        <div class="title">{{ title }}</div>
+        <div class="price">
+            <div class="price-normal">2499т</div>
+            <div class="price-discount">1990т</div>
+        </div>
+        <div class="add"><a href="#">Добавить в корзину</a></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductComponent',
+    props: {
+        imageId: Number,
+        title: String
+    },
+    methods: {
+        getImagePath() {
+            return require("../assets/products/" + this.imageId + '.png');
+        }
+    },
+}
+</script>
+
+<style scoped>
+.product {
+    width: 279px;
+    height: 468px;
+    border: 2px solid #DD926E;
+    border-radius: 24px;
+    box-sizing: border-box;
+    padding-top: 55px;
+    padding-left: 30px;
+    padding-right: 30px;
+}
+
+.product:hover {
+    background-color: #FFE5D9;
+    border: none;
+    box-sizing: border-box;
+}
+
+.image {
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+}
+
+.title {
+    margin-top: 38px;
+    text-align: center;
+    font-size: 15px;
+    color: #CF6736;
+}
+
+.price {
+    margin-top: 20px;
+}
+
+.price-normal {
+    color: #DD926E;
+    font-size: 16px;
+    text-decoration: line-through;
+    text-align: center;
+}
+
+.price-discount {
+    color: #CF6736;
+    font-size: 24px;
+    text-align: center;
+}
+
+.add {
+    margin-top: 20px;
+    font-size: 16px;
+    text-align: center;
+}
+
+a {
+    text-decoration: none;
+    color: #DD926E;
+}
+</style>
+
